9a5cfd9185172ff2ab595cf2d2abbe3247ea8eb6
Reed Law <reed@smashingboxes.com>
2013-11-07 22:05:44 -0500

    clean up

--- scene.cpp
+++ scene.cpp
@@ -14,12 +14,26 @@ TEST(Render, Positive) {
   ASSERT_EQ(1, 1);
 }
 
-TEST(Vector, Constructor) {
+TEST(Vector, Equality) {
   Vector v1(1.0f, 0.0f, 0.5f);
   Vector v2(1.0f, 0.0f, 0.5f);
   ASSERT_EQ(v1, v2);
 }
 
+TEST(Vector, Addition) {
+  Vector lhs(0.2f, 0.3f, 0.1f);
+  Vector rhs(0.3f, 0.2f, 0.0f);
+  Vector sum(0.5f, 0.5f, 0.1f);
+  ASSERT_EQ(lhs+rhs, sum);
+}
+
+TEST(Vector, Subtraction) {
+  Vector lhs(0.4f, 0.3f, 0.2f);
+  Vector rhs(0.3f, 0.2f, 0.1f);
+  Vector res(0.1f, 0.1f, 0.1f);
+  Vector result = lhs-rhs;
+  ASSERT_TRUE(lhs-rhs==res);
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
--- vector.cpp
+++ vector.cpp
@@ -1,15 +1,28 @@
 #include <gtest/gtest.h>
+#include "floating_point.h"
 
 class Vector {
 public:
   Vector(float x1, float y1, float z1);
   float x, y, z;
-  inline bool operator == (const Vector &b) const
+  typedef float RawType;      // float -> double for different output
+  inline bool operator== (const Vector &rhs) const
   {
-    return ( (b.x==x) &&
-             (b.y==y) &&
-             (b.z==z) );
+    return ( (FloatingPoint<RawType>(x).AlmostEquals(FloatingPoint<RawType>(rhs.x))) &&
+             (FloatingPoint<RawType>(y).AlmostEquals(FloatingPoint<RawType>(rhs.y))) &&
+             (FloatingPoint<RawType>(z).AlmostEquals(FloatingPoint<RawType>(rhs.z))) );
   }
+  inline Vector operator+ (const Vector &rhs) const
+  {
+    Vector ret (x+rhs.x, y+rhs.y, z+rhs.z);
+    return ret;
+  }
+  inline Vector operator- (const Vector &rhs) const
+  {
+    Vector ret (x-rhs.x, y-rhs.y, z-rhs.z);
+    return ret;
+  }
+
 private:
   float dir;
 };
